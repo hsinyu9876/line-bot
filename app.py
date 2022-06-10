a94fdd062a8d7f232a09bf0a57121daf2920599b
itshsinyu9876 <sa333559@gmail.com>
2022-06-10 16:56:29 +0800

    add reply stickers

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
 app = Flask(__name__)
@@ -37,9 +37,32 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '我看不懂你說什麼'
+
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='6362',
+            sticker_id='11087923'
+        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return#不回傳東西的回傳，為了結束function不執行以下if。
+
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰'
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位，是嘛?'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
